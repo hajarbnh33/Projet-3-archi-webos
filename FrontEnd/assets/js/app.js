0f020399699abcc86f6fb128f7ba881b513241ab
hajarbnh33 <hb-79@hotmail.fr>
2024-01-04 18:38:28 +0100

    modification CSS/JS/HTML

--- FrontEnd/assets/js/app.js
+++ FrontEnd/assets/js/app.js
@@ -40,3 +40,77 @@ function displayWorks(works) {
         container.appendChild(figure);
     }
 }
+
+// créer les boutons filtres
+/* 
+    <section id="portfolio">
+    <h2>Mes Projets</h2>
+    <div class="gallery"></div>
+    </section>
+*/
+//Je récupère mon élément du fichier HTML
+
+/*let classButton = document.querySelector(".buttonFilter");
+
+//J'indique le contenu de mes boutons
+let contenuFirstButton = "Tous";
+let contenuSecondButton = "Objets";
+let contenuThirdButton = "Appartements";
+let contenuFourthButton = "Hôtels & restaurants";
+
+// Je vais créer mes boutons filtres
+let firstButton = document.createElement("button");
+classButton.appendChild(firstButton);
+
+let secondButton = document.createElement("button");
+classButton.appendChild(secondButton);
+
+let thirdButton = document.createElement("button");
+classButton.appendChild(thirdButton);
+
+let fourthButton = document.createElement("button");
+classButton.appendChild(fourthButton);
+
+//J'ajoute le contenu aux boutons
+firstButton.textContent = contenuFirstButton;
+secondButton.textContent = contenuSecondButton;
+thirdButton.textContent = contenuThirdButton;
+fourthButton.textContent = contenuFourthButton;
+
+//La possibilité de filtrer la galerie par catégorie de projet
+
+//je récupère tous mes boutons
+let filters = document.querySelectorAll(".buttonFilter button");
+console.log(filters);
+
+//Boucle for of pour parcourir ma liste de filtre
+for (let filterButton of filters) {
+    filterButton.addEventListener("click", function () {
+        console.log("vous avez cliqué");
+    });
+}
+*/
+
+async function initFilter() {
+    let responseFilter = await fetch("http://localhost:5678/api/categories");
+    let filters = await responseFilter.json();
+    displayFilters(filters);
+    console.log(filters);
+}
+initFilter();
+
+function displayFilters(filters) {
+    console.log(filters);
+    const containerFilter = document.querySelector(".buttonFilter");
+    for (let filter of filters) {
+        console.log(filter);
+        //je crée mes balises boutons
+        const button = document.createElement("button");
+        containerFilter.appendChild(button);
+        //j'ajoute une class à chacun de mes filtres
+        const containerButton = document.querySelector("button");
+        button.classList.add("filtres");
+        //j'ajoute le texte dans mes filtres
+        button.innerText = filter.name;
+    }
+}
